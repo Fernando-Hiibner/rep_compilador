548feee9a044b4d85c110e02f772ba1fa5cb77b1
Fernando Hiibner <ferpinoti@gmail.com>
2022-03-23 22:15:52 -0300

    Finalizada aula dia 23/03

--- regras/__init__.py
+++ regras/__init__.py
@@ -6,7 +6,8 @@ REGRAS = {
     },
     "T" : {
         "id" : "T->FG",
-        "num": "T->FG"
+        "num": "T->FG",
+        "(" : "T->FG"
     },
     "S" : {
         "+" : "S->+TS",
@@ -28,3 +29,26 @@ REGRAS = {
         "(" : "(E)"
     }
 }
+
+"""
+(id + num)
+( - id - + - num - )
+
+REGRAS["F"]["("] -> (
+REGRAS["F"]["("] -> E["("] -> TS
+REGRAS["F"]["("] -> E["("] -> TS -> T["id"] -> FG
+REGRAS["F"]["("] -> E["("] -> TS -> T["id"] -> FG -> F["id"] -> True
+REGRAS["F"]["("] -> E["("] -> TS -> T["id"] -> FG -> G["+"]  -> lambda(+)
+REGRAS["F"]["("] -> E["("] -> TS -> T["id"] -> FG -> lambda(+)
+REGRAS["F"]["("] -> E["("] -> TS -> T["id"] -> lambda(+)
+REGRAS["F"]["("] -> E["("] -> TS -> S["lambda(+)"] -> +TS
+REGRAS["F"]["("] -> E["("] -> TS -> S["lambda(+)"] -> +TS["+"] -> True(+)
+REGRAS["F"]["("] -> E["("] -> TS -> S["lambda(+)"] -> +TS["num"] -> T["num"] -> FG
+REGRAS["F"]["("] -> E["("] -> TS -> S["lambda(+)"] -> +TS["num"] -> T["num"] -> FG -> F["num"] -> True
+REGRAS["F"]["("] -> E["("] -> TS -> S["lambda(+)"] -> +TS["num"] -> T["num"] -> FG -> True
+REGRAS["F"]["("] -> E["("] -> TS -> S["lambda(+)"] -> +TS["num"] -> T["num"] -> FG -> G[")"] -> lambda(")")
+REGRAS["F"]["("] -> E["("] -> TS -> S["lambda(+)"] -> +TS["num"] -> S[lambda(")")] -> lambda(")")
+REGRAS["F"]["("] -> E["("] -> TS -> lambda(")")
+REGRAS["F"]["("] -> E["("] -> lambda(")") -> True
+REGRAS["F"]["("] -> True
+"""
